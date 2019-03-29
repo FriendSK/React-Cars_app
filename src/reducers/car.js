e1288702f6c8cb3d3ea031e1f92a3408cc65aec9
FriendSK <49130sk@gmail.com>
2019-03-29 14:32:16 +0200

    'cars'

---
+++ src/reducers/car.js
@@ -0,0 +1,28 @@
+export default function () {
+    return [
+        {
+            id: 1,
+            name: 'Audi',
+            speed: 240.45,
+            weight: 1.4,
+            desc: 'RS7',
+            img: 'https://st.motortrend.com/uploads/sites/10/2016/05/2016-audi-rs7-hatchback-angular-front-2.png?interpolation=lanczos-none&fit=around|392:261'
+        },
+        {
+            id: 2,
+            name: 'BMW',
+            speed: 230.5,
+            weight: 1.2,
+            desc: 'BMW 5',
+            img: 'https://is4-ssl.mzstatic.com/image/thumb/Purple62/v4/bf/f8/e0/bff8e027-6ba9-83d8-66f4-f4a494ee77fc/source/256x256bb.jpg'
+        },
+        {
+            id: 3,
+            name: 'Mercedes',
+            speed: 220.10,
+            weight: 1.35,
+            desc: 'E class',
+            img: 'https://is2-ssl.mzstatic.com/image/thumb/Purple128/v4/82/7e/96/827e96fc-9111-f904-de08-e7ae7b50e90c/source/256x256bb.jpg'
+        }
+    ]
+}
